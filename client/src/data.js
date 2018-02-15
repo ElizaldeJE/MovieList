2ede466feed83a6208d8515c5a94cfab40c59be0
ElizaldeJE <javelisol@gmail.com>
2018-02-15 07:02:46 -0600

    created file /src/data.js which contains dummy data to prove that our data is showing as it should, the database has still not been created, this data is not showing in the page yet

---
+++ client/src/data.js
@@ -0,0 +1,22 @@
+/* This is dummy data of movies that could have been already
+seen, this is only for reference and proving that the data is
+flowing how it should.
+*/
+
+window.data = [{
+    description: 'DeadPool',
+    quantity: 4,
+  },
+  {
+    description: 'Ted',
+    quantity: 2
+  },
+  {
+    description: 'Ted 2',
+    quantity: 3
+  },
+  {
+    description: 'Green Street Hooligans',
+    quantity: 4
+  }
+]
