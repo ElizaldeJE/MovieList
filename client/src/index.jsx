795786b1503e077f54eb2308ebcfb76ec6c7c977
ElizaldeJE <javelisol@gmail.com>
2018-02-15 16:23:43 -0600

    it is showing in the local host with the background picture and title, still does not have the input boxes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,34 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from '/components/List.jsx';
+
+import MovieList from '/components/List.jsx';
+import AddMovie from '/components/AddItem.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       items: []
     }
+    this.AddMovieTitle = this.AddMovieTitle.bind(this);
+    this.getMovies = this.getMovies.bind(this);
   }
 
   componentDidMount() {
+    getMovies();
+}
+
+  AddMovieTitle(description, quantity){
+    console.log('des', description);
     $.ajax({
       url: '/items',
+      type: "POST",
+      contentType: 'application/json',
+      data: JSON.stringify({description: description, quantity: quantity}),
       success: (data) => {
-        this.setState({
-          items: data
-        })
+        this.getMovies();
       },
-      error: (err) => {
-        console.log('err', err);
+      error: (xhr, status, error) => {
+        console.log('err', xhr, status, error);
       }
-    });
-  }
+  })
+ }
+
+ getMovies(){
+   $.ajax({
+     url: '/items',
+     type: 'GET',
+     success: (data) => {
+       this.setState({list: data});
+     },
+     error: (xhr, err) => {
+       console.log('err', err);
+    }
+
+   })
+ }
+
+ componentDidMount(){
+   this.getMovies();
+ }
 
   render () {
     return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+      <h1>Movie List</h1>
+
+      <AddMovie AddMovieTitle={this.AddMovieTitle}/>
+      <MovieList list={this.state.list}/>
     </div>)
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var items = require('../database-mysql');
-
 var app = express();
 
-app.use(express.static(__dirname + '/../client/dist/index.html'));
 
+app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('/', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(200);
-    } else {
-      res.json(data);
-    }
-  });
-});
+//
+// app.get('/', function (req, res) {
+//   items.selectAll(function(err, data) {
+//     if(err) {
+//       res.sendStatus(200);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
