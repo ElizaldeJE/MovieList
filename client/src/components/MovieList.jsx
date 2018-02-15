4303172a00a251f1ef5bb7f2b12c35da4e4c5f28
ElizaldeJE <javelisol@gmail.com>
2018-02-15 06:44:29 -0600

    modified MovieList.jsx, it still needs lots of debugging work, check all the files carefully

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 import React from 'react':
 import MovieTitle from './MovieTitle.jsx';
 
-const MovieList = (props) => (
-  <div>
-    <h4>Movie List Component</h4>
-    There are {props.movies.length} movies.
-    {props.items.map(item => <MovieList item={item}/>)}
-  </div>
-)
+const MovieList = (props) =>
+    (<div className='movies'>
+        <h4>Movie List Component</h4>
+        There are {props.movies.length} movies.
+        {props.items.map((movie, i) =>
+          <MovieList movie={movie} key={i}/>)}
+     </div>);
+
 
 export default MovieList;
